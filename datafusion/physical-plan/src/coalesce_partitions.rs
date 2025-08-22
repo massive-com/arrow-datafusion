deaf2e2d6dc1ac8b0c293494a865546f1682c949
xudong963 <wxd963996380@gmail.com>
2025-08-22 17:18:21 +0800

    fix ut:custom_sources_cases::statistics::sql_limit(with_node_id of CoalescePartitionsExec missed fetch)

--- datafusion/physical-plan/src/coalesce_partitions.rs
+++ datafusion/physical-plan/src/coalesce_partitions.rs
@@ -217,6 +217,7 @@ impl ExecutionPlan for CoalescePartitionsExec {
         _node_id: usize,
     ) -> Result<Option<Arc<dyn ExecutionPlan>>> {
         let mut new_plan = CoalescePartitionsExec::new(Arc::clone(self.input()));
+        new_plan.fetch = self.fetch;
         let new_props = new_plan.cache.clone().with_node_id(_node_id);
         new_plan.cache = new_props;
         Ok(Some(Arc::new(new_plan)))
