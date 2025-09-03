70a3c94742a594cb7490b8d9ba4248990a24ec1d
Qi Zhu <qi.zhu@polygon.io>
2025-09-03 11:42:28 +0800

    fix clippy

--- datafusion/physical-optimizer/src/enforce_distribution.rs
+++ datafusion/physical-optimizer/src/enforce_distribution.rs
@@ -1031,6 +1031,7 @@ fn remove_dist_changing_operators(
 /// "    RepartitionExec: partitioning=RoundRobinBatch(10), input_partitions=2",
 /// "      DataSourceExec: file_groups={2 groups: \[\[x], \[y]]}, projection=\[a, b, c, d, e], output_ordering=\[a@0 ASC], file_type=parquet",
 /// ```
+#[allow(clippy::type_complexity)]
 pub fn replace_order_preserving_variants(
     mut context: DistributionContext,
     ordering_satisfied: bool,
